bad1391574423c4e36b2d9f43f9a4dec092aab80
Jason Fong <jasonfng7@gmail.com>
2022-10-12 03:15:42 -0400

    Working on Week6

--- app/src/main/java/com/example/fong_jason_lab5/MainActivity.java
+++ app/src/main/java/com/example/fong_jason_lab5/MainActivity.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jason Fong
+ * @version 1.0
+ */
 package com.example.fong_jason_lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,8 +11,13 @@ import com.example.fong_jason_lab5.databinding.ActivityMainBinding;
 import java.util.regex.Pattern;
 
 /**
+ * This class verifies the passwords entered by user
  * @author Jason Fong
- * @version 1.0
+ * @see com.example.fong_jason_lab5.databinding.ActivityMainBinding
+ * @see android.widget.Toast
+ * @see androidx.appcompat.app.AppCompatActivity
+ * @see android.os.Bundle
+ * @see java.util.regex.Pattern
  */
 public class MainActivity extends AppCompatActivity {
     /**
@@ -16,10 +25,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private ActivityMainBinding binding;
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?])" + ".{10,}" + "$");
-    /**
-     * Used to start an activity
-     * @param savedInstanceState Used to save & recover state information for your activity
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
